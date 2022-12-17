3b760c6d758e295bf53a3c9c43a595fc08b1a3c3
MercyObasi <mercyjohnsonone@gmail.com>
2022-12-17 13:05:55 +0100

    Automatic configuration with django-heroku

--- file_splitter/chunky/chunky/settings.py
+++ file_splitter/chunky/chunky/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 import environ
-import django_heroku
-django_heroku.settings(locals())
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env()
@@ -135,3 +133,5 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+"import django_heroku" 
+"django_heroku.settings(locals())" 
